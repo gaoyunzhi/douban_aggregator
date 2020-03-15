65e09e8928b7adbc1f8729dbbd870116235d3351
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-14 17:12:46 -0700

    commit

--- aggregate.py
+++ aggregate.py
@@ -17,18 +17,6 @@ from PIL import Image
 import export_to_telegraph
 import time
 
-page_start = 1
-page_end = 50
-try:
-	page_end = int(sys.argv[1])
-	try:
-		page_end = int(sys.argv[2])
-		page_start = int(sys.argv[1])
-	except:
-		pass
-except:
-	pass
-
 with open('credential') as f:
 	credential = yaml.load(f, Loader=yaml.FullLoader)
 export_to_telegraph.token = credential['telegraph_token']
@@ -112,42 +100,46 @@ def postTele(item, sid):
 					print(e)
 					print(images)
 
-r = None
-sids = set()
-for page in range(page_start, page_end):
-	print(page)
-	url = 'https://www.douban.com/?p=' + str(page)
-	content = getUrl(url)
-	b = BeautifulSoup(content, 'html.parser')
-	if not r:
-		r = BeautifulSoup(content, 'html.parser')
-		r_center = BeautifulSoup('<div id="wrapper" style="max-width:680px"></div>', features="lxml")
-		r.find('div', {'id': 'wrapper'}).replace_with(r_center)
-		r.find('div', class_='global-nav').decompose()
-		r.find('div', class_='nav').decompose()
-	r_center = r.find('div', {'id': 'wrapper'})
-	statuses = b.find('div', {'id': 'statuses'})
-	for item in statuses.find_all('div', class_='status-item'):
-		sid = item.attrs.get('data-sid')
-		if sid in sids:
-			continue
-		sids.add(sid)
-		if not wantSee(item, page):
-			continue
-		wr = BeautifulSoup('<div style="padding-bottom:30px"></div>', features="lxml")
-		wr.append(item)
-		r_center.append(wr)
-		postTele(item, sid) # TODO: dedup
-	for x in r.find_all('div', class_='actions'):
-		for y in x.find_all('a', class_='btn'):
-			y.decompose()
-		for y in x.find_all('span', class_='count'):
-			y.decompose()
-		for y in x.find_all('a'):
-			y.string = '----'
-	for x in r.find_all('blockquote'):
-		x['style'] = "max-height: 400px; display: block;"
-	with open('result.html', 'w') as f:
-		f.write(str(r))
-	if page % 5 == 0:
-		time.sleep(page % 31)
+def start():
+	r = None
+	sids = set()
+	for page in range(page_start, page_end):
+		print(page)
+		url = 'https://www.douban.com/?p=' + str(page)
+		content = getUrl(url)
+		b = BeautifulSoup(content, 'html.parser')
+		if not r:
+			r = BeautifulSoup(content, 'html.parser')
+			r_center = BeautifulSoup('<div id="wrapper" style="max-width:680px"></div>', features="lxml")
+			r.find('div', {'id': 'wrapper'}).replace_with(r_center)
+			r.find('div', class_='global-nav').decompose()
+			r.find('div', class_='nav').decompose()
+		r_center = r.find('div', {'id': 'wrapper'})
+		statuses = b.find('div', {'id': 'statuses'})
+		for item in statuses.find_all('div', class_='status-item'):
+			sid = item.attrs.get('data-sid')
+			if sid in sids:
+				continue
+			sids.add(sid)
+			if not wantSee(item, page):
+				continue
+			wr = BeautifulSoup('<div style="padding-bottom:30px"></div>', features="lxml")
+			wr.append(item)
+			r_center.append(wr)
+			postTele(item, sid) # TODO: dedup
+		for x in r.find_all('div', class_='actions'):
+			for y in x.find_all('a', class_='btn'):
+				y.decompose()
+			for y in x.find_all('span', class_='count'):
+				y.decompose()
+			for y in x.find_all('a'):
+				y.string = '----'
+		for x in r.find_all('blockquote'):
+			x['style'] = "max-height: 400px; display: block;"
+		with open('result.html', 'w') as f:
+			f.write(str(r))
+		if page % 5 == 0:
+			time.sleep(page % 31)
+
+if __name__ == '__main__':
+	start()
