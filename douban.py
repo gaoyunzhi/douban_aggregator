7b639685f7c33663b962056088840e058b5760e6
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-05 19:22:45 -0700

    commit

--- douban.py
+++ douban.py
@@ -72,7 +72,8 @@ def getResult(post_link, item):
 	if 'note' in post_link:
 		print('note found: ', not not note)
 		if not note:
-			print(item)
+			with open('tmp/note.html', 'w') as f:
+				f.write(item)
 	if note:
 		note = note['data-url']
 		url = export_to_telegraph.export(note, force=True)
